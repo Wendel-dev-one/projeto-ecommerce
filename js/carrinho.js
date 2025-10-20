44b1ac8aae0be2addf1f66413fc45f4b6412f0bb
Wendel-dev-one <wendel.04silva@gmail.com>
2025-10-19 21:39:57 -0300

    atualiza o contador do carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -56,6 +56,7 @@ botoesAdicionarAoCarrinho.forEach(botao => {
         }
 
         salvarProdutoNoCarrinho(carrinho);
+        atualizarContadorDoCarrinho();
     });
 });
 
@@ -67,3 +68,17 @@ function obterProdutosDoCarrinho() {
     const produtos = localStorage.getItem("carrinho");
     return produtos ? JSON.parse(produtos) : [];
 }
+
+// passo 4 - atualizar o contador do carrinho de compras 
+function atualizarContadorDoCarrinho () {
+    const carrinho = obterProdutosDoCarrinho();
+    let total = 0;
+    
+    carrinho.forEach(produto => {
+        total += produto.quantidade;
+    });
+
+    document.getElementById("contador-carrinho").textContent = total;
+}
+
+atualizarContadorDoCarrinho();
